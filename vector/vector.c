f13457e7599cc100ed3663de7c436c966c916010
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-05-09 10:06:19 +0900

    move from void** to void*

--- vector/vector.c
+++ vector/vector.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void mem_abort(void) {
 	fprintf(stderr, "error (re)allocating memory.¥n");
@@ -10,51 +11,51 @@ void mem_abort(void) {
 #define V_SCALE_FACTOR 1.5f
 //TODO: tweak values
 typedef struct vector {
-	void **items;
+	void *items;
 	size_t capacity;
 	size_t size;
+	size_t sizeof_type;
 } vector;
 
-void v_init_with_capacity(vector *v, size_t capacity) {
+void v_init_with_capacity(vector *v, size_t sizeof_type, size_t capacity) {
 	v->capacity = capacity;
 	v->size = 0;
-	v->items = calloc(v->capacity, sizeof(void *));
-	//This call to calloc() guarantees zero-initialization of vector contents.
-	//Use malloc() if it is unnecessary or performance is an issue.
+	v->sizeof_type = sizeof_type;
+	v->items = malloc(sizeof_type * capacity);
 	if (v->items == NULL) mem_abort();
 }
-void v_init(vector *v) {
-	v_init_with_capacity(v, V_INIT_CAP);
+void v_init(vector *v, size_t sizeof_type) {
+	v_init_with_capacity(v, sizeof_type, V_INIT_CAP);
 }
-unsigned int v_size(vector *v) {
-	return v->size;
+void *v_index(vector *v, size_t index) {
+	return (void *)((char *) v->items + v->sizeof_type * index);
 }
 static void v_resize(vector *v, size_t new_capacity) {
-	void **new_v_items = realloc(v->items, sizeof(void *) * new_capacity);
-	//TODO: check realloc side-effects (eg. zero initialization)
+	void *new_v_items = realloc(v->items, v->sizeof_type * new_capacity);
 	if (new_v_items == NULL) mem_abort();
 	v->items = new_v_items;
 	v->capacity = new_capacity;
 }
-void v_push_back(vector *v, void *item) {
-	if (v->capacity == v->size) v_resize(v, (size_t) (v->capacity * V_SCALE_FACTOR));
-	v->items[v->size] = item;
-	v->size++;
-}
 void v_set(vector *v, int index, void *item) {
-	v->items[index] = item;
+	memcpy(v_index(v, index), item, v->sizeof_type);
 }
 void *v_get(vector *v, int index) {
-	return v->items[index];
+	return v_index(v, index);
+}
+void v_push_back(vector *v, void *item) {
+	if (v->capacity == v->size) v_resize(v, (size_t) (v->capacity * V_SCALE_FACTOR));
+	v_set(v, v->size, item);
+	v->size++;
 }
 void v_delete(vector *v, int index) {
 	for(int i = index; i < v->size - 1; i++) {
-		v->items[i] = v->items[i + 1];
+		for(int j = 0; j < v->sizeof_type; i++) {
+			*((char *)(v_index(v, i)) + j) = *((char *)(v_index(v, i + 1)) + j);
+		}
 	}
-	v->items[v->size - 1] = NULL;
 	v->size--;
-	if (v->size > 0 && v->size == (size_t)((float)v->capacity / V_SCALE_FACTOR / V_SCALE_FACTOR)
-		vector_resize(v, v->capacity / V_SCALE_FACTOR);
+	if (v->size > 0 && v->size == (size_t)((float)v->capacity / V_SCALE_FACTOR / V_SCALE_FACTOR))
+		v_resize(v, v->capacity / V_SCALE_FACTOR);
 	//TODO: check if shrink routine works
 }
 void v_free(vector *v) {
