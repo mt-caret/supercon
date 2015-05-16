cf918039ec261d77492782b51448ca4ec817f319
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-05-17 08:58:23 +0900

    remove Makefile and fix stuff in vector.c

--- vector/vector.c
+++ vector/vector.c
@@ -8,8 +8,8 @@ void mem_abort(void) {
 }
 
 #define V_INIT_CAP 4
-#define V_SCALE_FACTOR 1.5f
-//TODO: tweak values
+#define V_SCALE_FACTOR 2 
+/*TODO: tweak values*/
 typedef struct vector {
 	void *items;
 	size_t capacity;
@@ -30,7 +30,7 @@ void v_init(vector *v, size_t sizeof_type) {
 void *v_index(vector *v, size_t index) {
 	return (void *)((char *) v->items + v->sizeof_type * index);
 }
-static void v_resize(vector *v, size_t new_capacity) {
+void v_resize(vector *v, size_t new_capacity) {
 	void *new_v_items = realloc(v->items, v->sizeof_type * new_capacity);
 	if (new_v_items == NULL) mem_abort();
 	v->items = new_v_items;
@@ -43,21 +43,27 @@ void *v_get(vector *v, int index) {
 	return v_index(v, index);
 }
 void v_push_back(vector *v, void *item) {
-	if (v->capacity == v->size) v_resize(v, (size_t) (v->capacity * V_SCALE_FACTOR));
+	if (v->capacity == v->size) v_resize(v, v->capacity * V_SCALE_FACTOR);
 	v_set(v, v->size, item);
 	v->size++;
 }
 void v_delete(vector *v, int index) {
-	for(int i = index; i < v->size - 1; i++) {
-		for(int j = 0; j < v->sizeof_type; i++) {
+  if (index != v-> size - 1) {
+		memmove(v_index(v, index), v_index(v, index + 1), v->sizeof_type * (v->size - index - 1));
+	}
+	/*int i;
+	for(i = index; i < v->size - 1; i++) {
+		int j;
+		for(j = 0; j < v->sizeof_type; i++) {
 			*((char *)(v_index(v, i)) + j) = *((char *)(v_index(v, i + 1)) + j);
 		}
-	}
+	}*/
 	v->size--;
-	if (v->size > 0 && v->size == (size_t)((float)v->capacity / V_SCALE_FACTOR / V_SCALE_FACTOR))
-		v_resize(v, v->capacity / V_SCALE_FACTOR);
-	//TODO: check if shrink routine works
+	/*if (v->size > 0 && v->size <= (size_t)(v->capacity / V_SCALE_FACTOR / V_SCALE_FACTOR))
+		v_resize(v, v->capacity / V_SCALE_FACTOR);*/
+	/*TODO: check if shrink routine works*/
 }
 void v_free(vector *v) {
 	free(v->items);
 }
+
