7c221b1eeb0b23dc60612c0e6eceeba3a179ad52
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-05-17 08:58:49 +0900

    add test code for vector

---
+++ vector/test/test.c
@@ -0,0 +1,24 @@
+#include "../vector.c"
+/*This include is used only for testing purposes!!!*/
+
+int main(void) {
+	vector v;
+	v_init(&v, sizeof(long));
+	int i;
+	for (i = 0; i < 50; i++)
+		v_push_back(&v, &i);
+	for (i = 0; i < 50; i++)
+		printf("%d\n", *((int *) v_get(&v, i)));
+  v_free(&v);
+
+  vector *v2;
+  v_init(v2, sizeof(char));
+	char *str = "Hello, World!";
+  for (i = 0; i < strlen(str); i++)
+		v_push_back(v2, str + i);
+  for (i = 0; i < v2->size; i++)
+		printf("%c", *((char *) v_get(v2, i)));
+  v_free(v2);
+	printf("\nCompleted!\n");
+	return 0;
+}
